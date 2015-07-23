7882e4cb032a3951cd95df5bb8b02fe4e270bfe4
Gunnar Schaefer <gsfr@stanford.edu>
2015-07-22 17:08:10 -0700

    some cleanup and improvement attempts

--- setup.py
+++ setup.py
@@ -5,23 +5,35 @@ version = '0.1'
 
 tests_require = []
 
-setup(name='scitran.reaper',
-      version=version,
-      description='Data collection service',
-      classifiers=[
-        "Programming Language :: Python",
+setup(
+    name='scitran.reaper',
+    version=version,
+    description='Data collection service',
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    keywords='scitran',
+    author='Gunnar Schaefer',
+    author_email='gsfr@stanford.edu',
+    url='http://scitran.github.io/',
+    packages=find_packages(),
+    namespace_packages=['scitran'],
+    install_requires=[
+        'pytz',
+        'tzlocal',
+        'requests',
+        'pydicom',
+        'numpy',
+        'nibabel',
+        'dcmstack',
+        'scitran.data',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dicom_net_reaper=scitran.reaper.dicom_reaper:main',
+            'dicom_file_reaper=scitran.reaper.dicom_file_reaper:main',
+            'pfile_reaper=scitran.reaper.pfile_reaper:main',
         ],
-      keywords='scitran',
-      author='Gunnar Schaefer',
-      author_email='gsfr@stanford.edu',
-      url='http://scitran.github.io/',
-      packages=find_packages(),
-      namespace_packages=['scitran'],
-      entry_points = {
-          'console_scripts':
-            ['dicom_net_reaper = scitran.reaper.dicom_reaper:main',
-            'dicom_file_reaper = scitran.reaper.dicom_file_reaper:main',
-            'pfile_reaper = scitran.reaper.pfile_reaper:main',
-            ]},
-      tests_require=tests_require,
-      )
+    },
+    tests_require=tests_require,
+)
