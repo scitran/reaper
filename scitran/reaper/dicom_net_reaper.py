7a24d5d129855aa274cde5d96a56ff9349ce0474
Gunnar Schaefer <gsfr@stanford.edu>
2016-01-12 17:38:42 -0800

    Fix patient id matching

--- scitran/reaper/dicom_net_reaper.py
+++ scitran/reaper/dicom_net_reaper.py
@@ -38,6 +38,8 @@ class DicomNetReaper(reaper.Reaper):
         'SeriesTime': '',
         'NumberOfSeriesRelatedInstances': '',
         'PatientID': '',
+        'OperatorsName': '',
+        'AccessionNumber': '',
     }
 
     def __init__(self, options):
@@ -76,7 +78,7 @@ class DicomNetReaper(reaper.Reaper):
         if success and reap_cnt > 0:
             dcm = self.DicomFile(filepaths[0])
             if not self.is_desired_patient_id(dcm.patient_id):
-            return None, {}
+                return None, {}
         if success and reap_cnt == item['state']['images']:
             acq_map = self.split_into_acquisitions(_id, item, tempdir, filepaths)
             metadata_map = {}
@@ -87,11 +89,11 @@ class DicomNetReaper(reaper.Reaper):
         else:
             return False, {}
 
-    def is_desired_patient_id(self, patient_id):
-        if not re.match(self.whitelist, patient_id):
+    def is_desired_patient_id(self, _id):
+        if not re.match(self.whitelist, _id):
             log.info('ignoring     %s (non-matching patient ID)' % _id)
             return False
-        if patient_id.strip('/').lower() in self.blacklist:
+        if _id.strip('/').lower() in self.blacklist:
             log.info('discarding   %s' % _id)
             return False
         return True
