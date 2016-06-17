ead3863686106b3a0bd243bbb6b55b0881ee146d
Gunnar Schaefer <gsfr@stanford.edu>
2016-06-16 17:09:39 -0700

    Add basic testing

--- bin/test.sh
+++ bin/test.sh
@@ -1,39 +1,44 @@
 #!/usr/bin/env bash
 
-set -e
+set -euv
 
 unset CDPATH
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
 
+# Set up exit and error trap to shutdown dependencies
+shutdown() {
+    echo 'Exit signal trapped'
+    kill $RECEIVER_PID
+    wait
+}
+trap "shutdown" EXIT ERR
+
+
+# Launch DICOM test server
 #dcmtk &
 #DCMTK_PID=$!
-#
-#upload_receiver.py &
-#RECEIVER_PID=$!
-#
-#reap
 
 
+# Launch dummy upload receiver
+python -m httpbin.core --port 8000 &
+RECEIVER_PID=$!
 
 
+# Fetch test data
+TEMPDIR=$(mktemp -d)
+curl -L https://github.com/scitran/testdata/archive/master.tar.gz | tar xz -C $TEMPDIR --strip-components 1
 
-## Set python path so scripts can work
-#export PYTHONPATH=.
-#
-#
-## Set up exit and error trap to shutdown mongod and paster
-#trap "{
-#    echo 'Exit signal trapped';
-#    kill $DCMTK_PID $RECEIVER_PID; wait;
-#    rm -f $TEMP_INI_FILE
-#    deactivate
-#}" EXIT ERR
-#
-#
-## Wait for everything to come up
-#sleep 2
-#
-#
-## Wait for good or bad things to happen until exit or error trap catches
-#wait
+
+# Test DICOM Sniper
+
+
+# Test DICOM Reaper
+
+
+# Test Folder Sniper
+folder_uploader -y $TEMPDIR http://localhost:8000/post
+
+
+# Cleanup
+rm -r $TEMPDIR
