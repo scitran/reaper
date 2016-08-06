8ab93b6399f0ba478f44399346023cd06f1e83bf
Gunnar Schaefer <gsfr@stanford.edu>
2016-08-05 20:27:28 -0700

    Break out CI linting
    Closes #36.

---
+++ test/lint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+
+unset CDPATH
+cd "$( dirname "${BASH_SOURCE[0]}" )/.."
+
+echo "Running pylint ..."
+pylint --reports=no reaper
+
+echo
+
+echo "Running pep8 ..."
+pep8 --max-line-length=150 --ignore=E402 reaper
