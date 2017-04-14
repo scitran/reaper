a5e9a032a006a55ac66506a3abff2079994084f3
Gunnar Schaefer <gsfr@flywheel.io>
2017-04-14 12:29:18 -0500

    Bump version for release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
 
 setup(
     name = 'reaper',
-    version = '1.0.5',
+    version = '1.0.7',
     description = 'SciTran Instrument Integration',
     author = 'Gunnar Schaefer',
     author_email = 'gsfr@stanford.edu',
