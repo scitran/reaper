4019df4915b641dbe0900a47edd5d7232b6632c3
Ambrus Simon <ambrussimon@invenshure.com>
2017-05-25 21:31:47 +0200

    make eeg detection (sort info inference) more flexible

--- reaper/eeg_reaper.py
+++ reaper/eeg_reaper.py
@@ -20,11 +20,12 @@ class EEGReaper(reaper.Reaper):
     """EEGReaper class"""
 
     def __init__(self, options):
-        if not os.path.isdir(options.get('path')):
+        path = options.get('path')
+        if not os.path.isdir(path):
             log.error('path argument must be a directory')
             sys.exit(1)
-        self.data_glob = os.path.join(options.get('path'), '*', '*', '*', '*', '*.eeg')
-        super(EEGReaper, self).__init__(options.get('path').strip('/').replace('/', '_'), options)
+        super(EEGReaper, self).__init__(path.strip('/').replace('/', '_'), options)
+        self.path = path
 
     def state_str(self, _id, state):
         return '%s, [%s, %s]' % (_id, state['mod_time'].strftime(reaper.DATE_FORMAT), util.hrsize(state['size']))
@@ -32,22 +33,26 @@ class EEGReaper(reaper.Reaper):
     def instrument_query(self):
         i_state = {}
         try:
-            filepaths = glob.glob(self.data_glob)
+            # list every path/**/*.eeg file (using os.walk because python2 glob doesn't support **)
+            filepaths = [fp for w in os.walk(self.path) for fp in glob.glob(os.path.join(w[0], '*.eeg'))]
             if not filepaths:
                 raise Warning('no matching files found (or error while checking for files)')
         except (OSError, Warning) as ex:
             filepaths = []
             log.warning(ex)
         for fp in filepaths:
-            eeg = EEGFile(fp)
-            i_state[eeg.reap_id] = reaper.ReaperItem(eeg.reap_state, path=fp)
+            try:
+                eeg = EEGFile(fp, self.path)
+            except (IOError, EEGFileError) as ex:
+                continue
+            i_state[eeg.acquisition_uid] = reaper.ReaperItem(eeg.reap_state, path=fp)
         return i_state
 
     def reap(self, _id, item, tempdir):
         try:
-            eeg = EEGFile(item['path'])
-        except AssertionError:
-            log.warning('skipping     %s (disappeared or unreadable)', _id)
+            eeg = EEGFile(item['path'], self.path)
+        except (IOError, EEGFileError) as ex:
+            log.warning('skipping     %s (disappeared or unparsable)', _id)
             return None, {}
 
         filepaths = sorted(glob.glob(os.path.splitext(item['path'])[0] + '.*'))
@@ -69,10 +74,15 @@ class EEGReaper(reaper.Reaper):
             log.warning('reap error   %s', _id)
             return False, None
         metadata = util.object_metadata(eeg, self.timezone, os.path.basename(filepath))
+        metadata['acquisition']['files'][0]['info'] = {'path': item['path']}
         util.set_archive_metadata(filepath, metadata)
         reap_time = (datetime.datetime.utcnow() - reap_start).total_seconds()
         log.info('reaped.zip   %s [%s] in %.1fs', _id, eeg_size, reap_time)
         return True, {filepath: metadata}
+
+
+class EEGFileError(Exception):
+    pass
 
 
 class EEGFile(object):
@@ -81,22 +91,41 @@ class EEGFile(object):
 
     # pylint: disable=too-few-public-methods
 
-    def __init__(self, filepath):
-        assert os.access(filepath, os.R_OK)
-        self.filepath = filepath
+    def __init__(self, filepath, reaperpath):
+        with open(filepath):
+            self.filepath = filepath
 
-        name = os.path.splitext(os.path.basename(filepath))[0]
-        self.acquisition_uid = name
+        creation_time = os.stat(filepath).st_ctime
+        relpath = os.path.relpath(filepath, reaperpath)
+        dirpath, filename = os.path.split(relpath)
+        hierarchy_info = dirpath.split('/') if dirpath else []
+        filename_info = os.path.splitext(filename)[0].split('_')
+        sort_info = hierarchy_info + filename_info
 
-        path = os.path.dirname(filepath)
-        path, self.session_uid = os.path.split(path)
-        path, self.subject_code = os.path.split(path)
-        path, self.project_label = os.path.split(path)
-        path, self.group__id = os.path.split(path)
+        # not enough data to infer all sorting levels
+        if len(sort_info) < 4:
+            # TBD should reaper support setting group[project] at startup?
+            # TBD these won't even be discovered making it hard to trace why it wasn't UL'd
+            raise EEGFileError('cannot infer sorting info from ' + relpath)
 
-        self.acquisition_timestamp = datetime.datetime.utcfromtimestamp(os.stat(filepath).st_ctime)
+        # autogenerate last level (acquisition_uid) if needed
+        if len(sort_info) < 5:
+            # TBD format, timezone?
+            dt = datetime.datetime.utcfromtimestamp(creation_time)
+            sort_info.append(dt.strftime('%Y%m%d_%H%M%S'))
+
+        # discard all but the last 5 sort info parts
+        if len(sort_info) > 5:
+            # TBD should this be handled differently?
+            # scenarios: hierarchy deep, too many _'s in filename or both
+            sort_info = sort_info[-5:]
+
+        sort_keys = ('group__id', 'project_label', 'subject_code', 'session_uid', 'acquisition_uid')
+        for i, sort_key in enumerate(sort_keys):
+            setattr(self, sort_key, sort_info[i])
+
+        self.acquisition_timestamp = datetime.datetime.utcfromtimestamp(creation_time)
         self.file_type = FILETYPE
-        self.reap_id = self.session_uid + self.acquisition_uid
 
     @property
     def reap_state(self):
@@ -110,7 +139,7 @@ class EEGFile(object):
 
 def update_arg_parser(ap):
     # pylint: disable=missing-docstring
-    ap.add_argument('path', help='path to EEG files')
+    ap.add_argument('path', help='path to "Raw Files"')
 
     return ap
 
